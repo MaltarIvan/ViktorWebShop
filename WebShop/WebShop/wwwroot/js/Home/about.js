97451f1b7e5cda6bf1c61d60d3a056524d6c154d
MaltarIvan <ivan.maltar@fer.hr>
2018-02-20 23:14:43 +0100

    added marker to google maps frame

--- WebShop/WebShop/wwwroot/js/Home/about.js
+++ WebShop/WebShop/wwwroot/js/Home/about.js
@@ -1,8 +1,14 @@
 ﻿function myMap() {
+    var latLng = { lat: 45.378724, lng: 13.921050};
     var mapOptions = {
-        center: new google.maps.LatLng(45.3799811, 13.9177101),
+        center: latLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'Istra Mushrooms'
+    });
 }
